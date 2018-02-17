96ff109bff851c454497c6079a17866c7ab9df3f
Ryan Rose <rytrose@gmail.com>
2018-02-17 19:33:40 +0000

    add p5

--- assets/js/music.js
+++ assets/js/music.js
@@ -14,8 +14,34 @@ window.addEventListener("beforeunload", function (event) {
 
 // On sound toggle
 $('#musictoggle').change(function() {
-      if($(this).prop('checked')){}
+      if(start) {
+        Tone.Transport.start();
+        bassPart.start(0);
+        start = false;
+      }
+      if($(this).prop('checked')) Tone.Master.mute = false;
+      else Tone.Master.mute = true;
 })
 
 // Music logic
-Tone.Transport.start();
+var start = true;
+
+var bass = new Tone.MonoSynth({
+			"volume" : -10,
+			"envelope" : {
+				"attack" : 0.2,
+				"decay" : 0.3,
+				"release" : 2,
+			},
+			"filterEnvelope" : {
+				"attack" : 0.001,
+				"decay" : 0.01,
+				"sustain" : 0.5,
+				"baseFrequency" : 200,
+				"octaves" : 2.6
+			}
+}).toMaster()
+
+var bassPart = new Tone.Sequence(function(time, note){
+	bass.triggerAttackRelease(note, "16n", time);
+}, ["C2", ["C3", ["C3", "D2"]], "E2", ["D2", "A1"], "G1"]);
